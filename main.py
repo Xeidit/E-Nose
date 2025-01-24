1f796791ebb83395b0e22f328f34843d91feb958
andrew <xeidit@gmail.com>
2025-01-24 20:01:13 +0000

    Added Classes

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-class machine:
+import serial
+
+class Machine:
     def __init__(self):
         self._position = [0,0,0] # X Y Z
+
+    def homeMachine (self):
+        command = "G28"
+        printer.write(command.encode())
+        response = printer.readline().decode()
+        print(response)
 
 class Bottle:
     def __init__(self):
@@ -11,3 +19,9 @@ class Bottle:
 class Chemical:
     def __init__(self):
         self._name = "None"
+        self._description = "Something Interesting"
+
+# Create a machine if one does not exist
+
+# Connect to machine via serial
+printer = serial.Serial('COM3', 9600)
