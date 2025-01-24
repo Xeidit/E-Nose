d739adef1df86094e5e57174e5b0b0c43058e643
andrew <xeidit@gmail.com>
2025-01-24 19:45:38 +0000

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class machine:
+    def __init__(self):
+        self._position = [0,0,0] # X Y Z
+
+class Bottle:
+    def __init__(self):
+        self._size = [0,0] # X Y
+        self._position = [0,0] # X Y
+        self.content = ""
+
+class Chemical:
+    def __init__(self):
+        self._name = "None"
