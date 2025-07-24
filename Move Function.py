f82fc62e1b4c6abda54844911ded291e4e6c13e1
Xeidit <Xeidit@gmail.com>
2025-07-24 13:40:16 +0100

    Combined types of movement into 1

--- Move Function.py
+++ Move Function.py
@@ -10,15 +10,17 @@ from standard_fuctions import g_code
 safe_z_height = str(255) # Safe height for the head to move around at without the poker hitting bottles
 
 
-def abs_move(url,x,y,z):
+def abs_move(url,type,x,y,z):
+    if type == "rel":
+        g_code(url,"G91") # Sets extruder mode to relative
+        g_code(url,"m82") # Keeps the extruder in absolute mode
     if x==0 and y==0:
         command = "G01" + "x0" + "y0" +"z" + z
         g_code(url,command)
-        return "ok"
+
     else:
         g_code(url,"G01 Z"+safe_z_height)
         g_code(url,"G01" + "x"+ x + "y" + y +"z" + z)
-        return "ok"
 
-def rel_move(url,x,y,z):
-    command = "G01" + "x" + x +"y" + y +"z" + z
+    g_code(url, "g90") # Sets all axis to absolute
+    return "ok"
