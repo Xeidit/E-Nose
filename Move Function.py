7c9d3ad9358565736629f548f7c27ed8c7d1f2df
Xeidit <Xeidit@gmail.com>
2025-07-25 10:47:00 +0100

    Cleaned up code

--- Move Function.py
+++ Move Function.py
@@ -1,18 +1,10 @@
-'''
-jobs to do:
-check movement wont foul endstops (check position of head)
-move head (make sure z_min is reached
-report new position
-
-'''
 
 from standard_fuctions import g_code
 safe_z_height = str(255) # Safe height for the head to move around at without the poker hitting bottles
 
-
-def abs_move(url,type,x,y,z):
-    if type == "rel":
-        g_code(url,"G91") # Sets extruder mode to relative
+def abs_move(url,move_type,x,y,z):
+    if move_type == "rel":
+        g_code(url,"g91") # Sets extruder mode to relative
         g_code(url,"m82") # Keeps the extruder in absolute mode
     if x==0 and y==0:
         command = "G01" + "x0" + "y0" +"z" + z
